cdc639ca19421dcbb9f66c81d8cc14c49e23ca7d
Dipal Das <dasdipal8888@gmail.com>
2025-12-17 19:55:34 +0530

    Implement OrderController: add createOrder, getAllOrders, and getMyOrders functions

---
+++ backend/src/controllers/OrderController.js
@@ -0,0 +1,61 @@
+const Order = require("../models/OrderModel");
+
+const createOrder = async (req, res) => {
+  try {
+    const userId = req.user.Id;
+    const { items, totalAmount } = req.body;
+    const order = await Order.create({
+      user: userId,
+      items,
+      totalAmount,
+      status: "completed",
+    });
+
+    res.status(201).send({
+      message: "Order created Successfully",
+      Order: Order,
+    });
+  } catch (error) {
+    console.log("Error occured", error);
+    res.status(500).send({
+      message: "Failed to create Order",
+    });
+  }
+};
+
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("user", "name email role")
+      .populate("items.books", "title newPrice")
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      message: "All orders fetched Successfully",
+      orders,
+    });
+  } catch (error) {
+    console.log("Error fetching Orders", error);
+    res.status(500).send({
+      message: "Failed to fetch orders",
+    });
+  }
+};
+const getMyOrders = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const orders = await Order.find({ user: userId })
+      .populate("items.books", "title newPrice coverImage")
+      .sort({ createdAt: -1 });
+  } catch (error) {
+    console.log("Error fetching user Orders", error);
+    res.status(500).send({
+      message: "Failed to fetch orders",
+    });
+  }
+};
+
+module.exports = {
+  createOrder,
+  getAllOrders,
+  getMyOrders,
+};
